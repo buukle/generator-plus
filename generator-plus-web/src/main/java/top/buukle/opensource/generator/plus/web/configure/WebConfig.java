17bc25d5a36f80d34027cae293f48832041371c2
elvin <elvin@18001268330@163.com>
2021-11-15 23:51:31 +0800

    fix

--- generator-plus-web/src/main/java/top/buukle/opensource/generator/plus/web/configure/WebConfig.java
+++ generator-plus-web/src/main/java/top/buukle/opensource/generator/plus/web/configure/WebConfig.java
@@ -7,29 +7,26 @@ import top.buukle.opensource.generator.plus.utils.SystemUtil;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
-	    @Override
-        public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            registry.addResourceHandler("/**")
-                    .addResourceLocations("classpath:/resources/")
-                    .addResourceLocations("classpath:/static/")
-                    .addResourceLocations("classpath:/public/");
-            registry.addResourceHandler("swagger-ui.html").addResourceLocations(
-                    "classpath:/META-INF/resources/");
-            registry.addResourceHandler("doc.html").addResourceLocations(
-                    "classpath:/META-INF/resources/");
-            registry.addResourceHandler("/webjars/**").addResourceLocations(
-                    "classpath:/META-INF/resources/webjars/");
-	        if(SystemUtil.isWindows()){
-                registry.addResourceHandler("/upload/**")
-                        .addResourceLocations("file:D://file/temp");
-                registry.addResourceHandler("/generator-plus/**")
-                        .addResourceLocations("file:C:/Users/17600/Desktop/elvin/generator-plus.all/generator-plus-front/dist/");
-            }else{
-                registry.addResourceHandler("/upload/**")
-                        .addResourceLocations("file:/opt/temp");
-                registry.addResourceHandler("/generator-plus/**")
-                        .addResourceLocations("file:/root/webrun/front/generator-plus-front/dist/");
-            }
-            super.addResourceHandlers(registry);
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/") .addResourceLocations("classpath:/static/").addResourceLocations("classpath:/public/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations( "classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/generator-plus/**")
+                .addResourceLocations("classpath:/static/");
+
+        if(SystemUtil.isWindows()){
+            registry.addResourceHandler("/upload/**")
+                    .addResourceLocations("file:D://file/temp");
+        }else{
+            registry.addResourceHandler("/upload/**")
+                    .addResourceLocations("file:/opt/temp");
         }
+        super.addResourceHandlers(registry);
+    }
 }
